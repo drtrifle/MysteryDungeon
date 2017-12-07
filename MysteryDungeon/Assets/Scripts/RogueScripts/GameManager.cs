92221a91b119d9760cc03c2732a63406465e7a42
drtrifle <drtrifle@gmail.com>
2017-12-07 12:25:54 +0800

    Refactoring & Optimisation

--- MysteryDungeon/Assets/Scripts/RogueScripts/GameManager.cs
+++ MysteryDungeon/Assets/Scripts/RogueScripts/GameManager.cs
@@ -27,16 +27,14 @@ public class GameManager : MonoBehaviour {
     //Awake is always called before any Start functions
     void Awake() {
         //Check if instance already exists
-        if (instance == null)
-
+        if (instance == null) {
             //if not, set instance to this
             instance = this;
-
-        //If instance already exists and it's not this:
-        else if (instance != this)
-
+        } else if (instance != this) {
+            //If instance already exists and it's not this:
             //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
             Destroy(gameObject);
+        }
 
         //Sets this to not be destroyed when reloading scene
         DontDestroyOnLoad(gameObject);
